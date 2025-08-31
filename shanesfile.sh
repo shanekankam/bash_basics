5eae28b380460a094dcb0746be60632bdffc9343
Shane Kankam <skankam@ggc.edu>
2025-08-31 01:32:41 -0400

    Shane's name

---
+++ shanesfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello Shane Kankam!"
+echo "How old are you?" 
+read age
+echo 'Thanks Shane! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' $school ' sounds like a great place to learn.'
